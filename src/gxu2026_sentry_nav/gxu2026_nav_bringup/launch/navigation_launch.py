da369caa483d2bcc279c5fd624917970eaf8ebbe
Alphabet <bwyoung252006@gmail.com>
2026-03-06 19:25:25 +0800

    给地形分析加开关 暂时不启用

--- src/gxu2026_sentry_nav/gxu2026_nav_bringup/launch/navigation_launch.py
+++ src/gxu2026_sentry_nav/gxu2026_nav_bringup/launch/navigation_launch.py
@@ -157,6 +157,12 @@ def generate_launch_description():
     enable_obstacle_scan = LaunchConfiguration("enable_obstacle_scan")
     enable_terrain_analysis = LaunchConfiguration("enable_terrain_analysis")
 
+    declare_enable_terrain_analysis_cmd = DeclareLaunchArgument(
+        "enable_terrain_analysis",
+        default_value="true",
+        description="Whether to launch terrain_analysis and terrain_analysis_ext nodes",
+    )
+
     start_pointcloud_to_laserscan_cmd = Node(
         package="pointcloud_to_laserscan",
         executable="pointcloud_to_laserscan_node",
@@ -766,7 +772,7 @@ def generate_launch_description():
     # processed params defaults to original params file
     ld.add_action(SetLaunchConfiguration("processed_params_file", params_file))
     ld.add_action(SetLaunchConfiguration("enable_obstacle_scan", "false"))
-    ld.add_action(SetLaunchConfiguration("enable_terrain_analysis", "true"))
+    ld.add_action(declare_enable_terrain_analysis_cmd)
     # Set switches before starting nodes
     ld.add_action(set_switches_cmd)
     # Add the actions to launch all of the navigation nodes
