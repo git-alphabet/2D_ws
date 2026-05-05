1163abba4983f69af200214dc631e267fae7ede1
Boyu Jia <kukd8meop@gmail.com>
2026-05-05 18:50:09 +0800

    删除堡垒区点位

--- src/RM_Behavior_Tree/rm_behavior_tree/src/rm_behavior_tree.cpp
+++ src/RM_Behavior_Tree/rm_behavior_tree/src/rm_behavior_tree.cpp
@@ -137,7 +137,6 @@ int main(int argc, char ** argv)
       "home_x","home_y","supply_zone_x","supply_zone_y",
       "base_x","base_y",
       "base_buff_x","base_buff_y","outpost_buff_x","outpost_buff_y",
-      "fortress_ally_x","fortress_ally_y",
       "central_highland_x","central_highland_y",
       "ladder_highland_x","ladder_highland_y",
       "defend_anchor_x","defend_anchor_y",
